63a2554c6694741624e71ad4064f35bca45f1f8e
vitor9998 <vitor9998@gmail.com>
2019-12-09 15:07:57 -0300

    Add files via upload

--- service.py
+++ service.py
@@ -5,9 +5,37 @@ import unicodedata
 class Filmes:
     def filtro(nome):
         nome = str(nome.replace(" ", "-"))
-        URL = 'https://swapi.co/api/' + nome
+
+        base_url = 'https://swapi.co/api/people/?search='
+        URL = (base_url + nome)
+        
         resposta = requests.get(URL)
         lista_de_filmes = resposta.text
         joson_retorno = json.loads(lista_de_filmes)
 
-        return joson_retorno
+       
+
+
+        A=[]
+        
+        for OBJETO in joson_retorno ['results']:
+            B={}
+            B['nome'] = OBJETO['name']
+            B['altura'] = OBJETO['height']
+            B['peso'] = OBJETO['mass']
+            B['cor_do_cabelo'] = OBJETO['hair_color']
+            B['cor_da_pele'] = OBJETO['skin_color']
+            B['cor_do_olho'] = OBJETO['eye_color']
+            B['data_nascimento'] = OBJETO['birth_year']
+            B['sexo'] = OBJETO['gender']
+            B['filmes'] = OBJETO['films']
+            B['veículo'] = OBJETO['species']
+            B['nave'] = OBJETO['starships']
+            B['criação'] = OBJETO['species']
+            B['editado'] = OBJETO['edited']
+            
+            
+            A.append(B)
+            
+
+        return A
