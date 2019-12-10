b6037aed73110225b1a31e7cd9d28aedc5a9cf3e
vitor9998 <vitor9998@gmail.com>
2019-12-10 16:36:16 -0300

    Add files via upload

--- service.py
+++ service.py
@@ -29,16 +29,77 @@ class Filmes:
             B['data_nascimento'] = result_json ['birth_year']
         if 'gender' in result_json and result_json['gender']:
             B['genêro'] = result_json ['gender']
-        if 'homeworld' in result_json and result_json['homeworld']:
-            B['mundo_natal'] = result_json ['homeworld']
+        
+
+        if'homeworld'in result_json and result_json['homeworld']:
+            _mundo_natal = {}
+            resposta_mundo_natal = requests.get(result_json['homeworld'])
+            mundo_json = json.loads(resposta_mundo_natal.text)
+            _mundo_natal['Nome'] = mundo_json['name']
+            _mundo_natal['Terreno'] = mundo_json['terrain']
+            B['Mundo_Natal'] = _mundo_natal
+
+
         if 'films' in result_json and result_json['films']:
-            B['filmes'] = result_json ['films']
+            films = []              
+            for OBJETO in result_json['films']:
+                C = {}
+                resposta_filmes = requests.get(OBJETO)
+                film_json = json.loads(resposta_filmes.text)
+                C['Título'] = film_json['title']
+                C['Episódio'] = film_json['episode_id']
+                films.append(C)
+            B['Filmes'] = films
+        
+
+
+
         if 'species' in result_json and result_json['species']:
-            B['espécies'] = result_json ['species']
+            especie = []
+            for OBJETO2 in result_json['species']:
+                D={}
+                reposta_epecies = requests.get(OBJETO2)
+                especie_json = json.loads(reposta_epecies.text)
+                D['Nome'] = especie_json['name']
+                D['classificação'] = especie_json['classification']
+                D['Língua'] = especie_json['language']
+                especie.append(D)
+                
+            B['espécies'] = especie
+
+
+
         if 'vehicles' in result_json and result_json['vehicles']:
-            B['veículos'] = result_json ['vehicles']
+            veiculo = []
+            for OBJETO3 in result_json['vehicles']:
+                E ={}
+                resposta_veiculo = requests.get(OBJETO3)
+                veiculo_json = json.loads(resposta_veiculo.text)
+                E['Nome'] = veiculo_json['name']
+                E['Modelo'] = veiculo_json['model']
+                veiculo.append(E)
+
+
+            B['veículos'] = veiculo
+
+
         if 'starships' in result_json and result_json['starships']:
-            B['nave'] = result_json ['starships']
+            nave = []
+            for OBJETO4 in result_json['starships']:
+                F={}
+                resposta_nave = requests.get(OBJETO4)
+                nave_json = json.loads(resposta_nave.text)
+                F['Nome'] = nave_json['name']
+                F['Modelo'] = nave_json['model']
+                F['Passageiros'] = nave_json['passengers']
+
+                nave.append(F)
+
+
+
+            B['nave'] = nave
+
+
         if 'created' in result_json and result_json['created']:
             B['criação'] = result_json ['created']
         if 'edited' in result_json and result_json['edited']:
---
+++ utilidade.py
@@ -0,0 +1,9 @@
+def sexo(text):
+   if text == 'male':
+      text = 'Masculino'
+   elif text == 'female':
+      text = 'Feminino'
+   else:
+      text = text 
+ 
+   return text
